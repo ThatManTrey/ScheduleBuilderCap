15225e1bc9116af8ed211171a53169232499fca9
jaimie453 <jaimie6453@gmail.com>
2021-02-16 22:51:05 -0500

    added login prototype

--- flask_app.py
+++ flask_app.py
@@ -1,10 +1,14 @@
 # A very simple Flask Hello World app for you to get started with...
 
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
+def index():
     return 'Hello from Flask!'
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
 
