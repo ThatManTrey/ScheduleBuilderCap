64afa5b456e7ff41050dc97ddd01d914c89dfcf4
dstepha3 <62953513+dstepha3@users.noreply.github.com>
2021-02-18 00:42:39 -0500

    added favorites and about page, redirects in nav bar

--- flask_app.py
+++ flask_app.py
@@ -18,3 +18,11 @@ def login():
 @app.route('/register')
 def register():
     return render_template('register.html', title="Register")
+
+@app.route('/favorites')
+def favorites():
+    return render_template('favorites.html', title="Favorites")
+
+@app.route('/about')
+def about():
+    return render_template('about.html', title='About')
