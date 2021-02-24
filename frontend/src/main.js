ee835d165f87effc322fcc18b15595d3f4ab1ccc
jaimie453 <jaimie6453@gmail.com>
2021-02-23 21:48:53 -0500

    installed packages and static files, removed old files

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,5 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
+import './assets/style.css'
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/js/bootstrap.js';
+import '@fortawesome/fontawesome-free/js/all.js';
 
 Vue.config.productionTip = false
 
