fda1e25ce16afbf19255c6b6effc00a5d1e87d95
jaimie453 <jaimie6453@gmail.com>
2021-02-23 22:15:57 -0500

    router setup

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -4,9 +4,11 @@ import './assets/style.css'
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/js/bootstrap.js';
 import '@fortawesome/fontawesome-free/js/all.js';
+import router from './router'
 
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
+  router,
+  render: h => h(App)
 }).$mount('#app')
