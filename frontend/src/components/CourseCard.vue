c6e9b1db5ea1182cfdc52f0ea8324c8fec57ab69
dstepha3 <62953513+dstepha3@users.noreply.github.com>
2021-02-25 18:39:49 -0500

    adding course modal

--- frontend/src/components/CourseCard.vue
+++ frontend/src/components/CourseCard.vue
@@ -1,5 +1,7 @@
 <template lang="html">
-  <div class="card justify-content-center bg-theme-blacker course-card">
+  <div class="card justify-content-center bg-theme-blacker course-card" 
+                data-bs-toggle="modal"
+                data-bs-target="#exampleModalTwo" >
     <div class="card-body container-fluid text-theme-whiter">
       <div class="row text-theme-whitest">
         <h4 class="course-card-title">Analytic Geometry And Calculus I</h4>
@@ -27,5 +29,92 @@
         </div>
       </div>
     </div>
+
+    <div
+          class="modal fade"
+          id="exampleModalTwo"
+          tabindex="-1"
+          aria-labelledby="exampleModalLabelTwo"
+          aria-hidden="true"
+        >
+          <div class="modal-dialog">
+            <div class="theme-modal-content">
+              <div class="theme-modal-header">
+                <h5 class="modal-title" id="exampleModalLabelOne">
+                  Analytical Geometry And Calculus I
+                </h5>
+                <button
+                  type="button"
+                  class="button-as-link"
+                  data-bs-dismiss="modal"
+                >
+                  <i class="fas fa-times fa-lg text-theme-lightest-gray"></i>
+                </button>
+              </div>
+              <div class="theme-modal-body">
+                <div style="text-align: center">
+                  <span class="badge rounded-pill course-badge">MATH13013</span>
+                  <span class="badge rounded-pill course-badge">4 Credits</span>
+                  <br/><br/>
+                </div>
+                <p>
+                  This course will introduce the state-of-art computing platforms with
+                  the focus on how to utilize them in processing (managing and
+                  analyzing) massive datasets...
+                </p>
+                <p>
+                  Prerequisite:
+                </p>
+                <p>
+                  Schedule Type:
+                </p>
+                <p>
+                  Contact Hours:
+                </p>
+                <p>
+                  Grade Mode:
+                </p>
+                <div class="row" style="text-align: center">
+                   <hr/>
+                  <div class="col">
+                    <p>Course Quality:</p>
+                    <p>
+                      <i class="fas fa-star fa-lg text-theme-secondary"></i>
+                      <i class="far fa-star fa-lg text-theme-secondary"></i>
+                      <i class="far fa-star fa-lg text-theme-secondary"></i>
+                      <i class="far fa-star fa-lg text-theme-secondary"></i>
+                      <i class="far fa-star fa-lg text-theme-secondary"></i>
+                    </p>
+                  </div>
+                  <div class="col">
+                    <p>Course Difficulty:</p>
+                    <p>
+                      <i class="fas fa-star fa-lg text-theme-secondary"></i>
+                      <i class="far fa-star fa-lg text-theme-secondary"></i>
+                      <i class="far fa-star fa-lg text-theme-secondary"></i>
+                      <i class="far fa-star fa-lg text-theme-secondary"></i>
+                      <i class="far fa-star fa-lg text-theme-secondary"></i>
+                    </p>
+                  </div>
+                </div>
+                <div style="text-align: center">
+                  <button type="button" class="btn btn-theme-secondary" style="justify-content:center">
+                    Rate Course
+                  </button>
+                </div>
+              </div>
+              <div class="row" style="margin:1rem">
+                <div class="col">
+                    <a href="#"><i class="far fa-star fa-lg text-theme-secondary"></i></a>
+                  </div>
+                <div class="col" style="text-align: right;">
+                  <a href="#"><i class="fas fa-plus-circle fa-lg text-theme-confirm"></i></a>
+        </div>
+      </div>
+            </div>
+          </div>
+        </div>
+
   </div>
+
 </template>
