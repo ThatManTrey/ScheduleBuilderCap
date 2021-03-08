8c2a7c9edbda50a35db3b5028131983caeb566a3
ThatManTrey <tphillips_1969@yahoo.com>
2021-03-07 22:23:34 -0500

    updated packages, retrieved most of course data to submit query

--- scraper/scraperBot.py
+++ scraper/scraperBot.py
@@ -1,3 +1,4 @@
+# 
 
 #------------------------------------------------------------------------------
 # modules
@@ -28,10 +29,10 @@ def scrape():
     #delete tables?
     
     getAllCourses()
-    getPrograms()
+    getAllPrograms()
 
 #------------------------------------------------------------------------------
-# traversal functions
+# data retrieval
 
 # get all courses, add each to database
 def getAllCourses():
@@ -42,14 +43,14 @@ def getAllCourses():
     atozIndex = coursesSite.find(id="atozindex")
 
     # find all lettered lists as <ul>
-    letterList = deptList.find_all('ul')
+    letterList = atozIndex.find_all('ul')
     
     # get lists of depts as <a> tags
     deptList=[]
     for letter in letterList:
         deptList.append(letter.find_all('a'))
     
-    # open each dept <a> tag to get courses fro that dept
+    # open each dept <a> tag to get courses from that dept
     for dept in deptList:
         getdeptCourses(dept)
 
@@ -58,7 +59,7 @@ def getAllCourses():
 # given <a href="/coursesaz/DEPT/">DEPT_NAME (ABBREVIATION)</a>
 def getDeptCourses(dept):
     # get dept courses page data
-    deptCoursesSite = getSiteData('http://catalog.kent.edu/' + dept.get('href'))
+    deptCoursesSite = getSiteData('http://catalog.kent.edu' + dept.get('href'))
 
     # get main text
     deptContent = deptCoursesSite.find('div', class_="sc_sccoursedescs")
@@ -70,22 +71,93 @@ def getDeptCourses(dept):
     for course in courses:
         getCourseData(course)
 
+
+# NEED TO DO CORE PART
 # takes in html class "courseblock" inside html class "sc_sccoursedescs"
+# http://catalog.kent.edu/coursesaz/acct/
 def getCourseData(course):
-    ##################stopped here lol
-    #http://catalog.kent.edu/coursesaz/acct/
+    # get all <p> tags
+    attributes = course.find_all('p')
+    
+    # title (need to account for cores in name)
+    # see http://catalog.kent.edu/coursesaz/hist/
+    titleText = attributes[0].find('strong').get_text().split(" ")
+    CourseID_Type = titleText[0]
+    CourseID = CourseID_Type + " " + titleText[1]
+    
+    CourseName = ""
+    for word in range(2, len(titleText)-3):
+        CourseName = CourseName + word + " "
+    
+    # description (might need to account for '"'?)
+    CourseDesc = attributes[1].get_text()
+    firstPar = CourseDesc.find("(")
+    secondPar = CourseDesc.find(")")
+    for i in range(firstPar, secondPar+1)
+        del CourseDesc[i]
+    
+    # course type (might need to account for '"'?)
+    attributes[3].find('strong').clear()
+    CourseType = attributes[3].get_text()
 
-def getPrograms():
+    # credit hours
+    attributes[4].find('strong').clear()
+    CreditHours = attributes[4].get_text()[0]
+
+    # grade type
+    attributes[5].find('strong').clear()
+    GradeType = attributes[5].get_text()
+
+    # insert into db
+    addCourse(CourseID, CourseName, CourseDesc, CourseType, CreditHours, GradeType, CourseID_Type, KentCore)
 
 
+# get all programs, add each to database
+def getAllPrograms():
+    # get program page data
+    programsSite = getSiteData('http://catalog.kent.edu/programsaz/')
+    
+    #find the meat of the page #az_sitemap
+    az_sitemap = programsSite.find(id="az_sitemap")
 
-def getProgramReqs():
+    # find all lettered lists as <ul>
+    letterList = az_sitemap.find_all('ul')
+    
+    # get lists of programs as <a> tags
+    programList=[]
+    for letter in letterList:
+        programList.append(letter.find_all('a'))
+    
+    # open each program <a> tag to get courses from that dept
+    for program in programList:
+        getProgramConcentrations(program)
+
+
+# I AM HERE gets all courses in dept page, adds each to database
+# given <a href="/colleges/COLLEGE/DEPT-SCHOOL/">PROGRAM_NAME</a>
+def getProgramConcentrations(program):
+    # get dept courses page data
+    programCoursesSite = getSiteData('http://catalog.kent.edu' + program.get('href') + "#programrequirementstext")
+
+    # get main text
+    deptContent = deptCoursesSite.find('div', class_="sc_sccoursedescs")
+
+    # get courses list
+    courses = deptContent.find_all('div', class_="courseblock")
+
+    # get data for each concentration, 
+    for course in courses:
+        getCourseData(course)
+
 
 #------------------------------------------------------------------------------
 # database shit
-def addCourse()
 
-def addProgram() #?
+# adds course record in db
+def addCourse(CourseID, CourseName, CourseDesc, CourseType, CreditHours, GradeType, CourseID_Type, KentCore):
+    
+
+def addProgram():
 
 #------------------------------------------------------------------------------
 # misc
