95807c68d45591aa402ff671c7395716977fcb7a
jaimie453 <jaimie6453@gmail.com>
2021-03-10 12:43:55 -0500

    hiding page routes locally

--- backend/flask_app.py
+++ backend/flask_app.py
@@ -5,9 +5,9 @@ from flask import Flask, jsonify, render_template, send_from_directory
 from flask_cors import CORS
 
 app = Flask(__name__,
-    template_folder="../frontend/dist/",
-    static_folder="../frontend/dist/",
-)
+            template_folder="../frontend/dist/",
+            static_folder="../frontend/dist/",
+            )
 # enable CORS
 CORS(app, resources={r'/*': {'origins': '*'}})
 
@@ -83,50 +83,61 @@ accent_colors = [
 # requires npm run build to be run in /frontend first
 #
 
-@app.route('/')
-def index():
-    try:
-        return render_template('index.html')
-    except:
-        return "There is not currently a /frontend/dist folder for the built frontend files. Type <b>npm run build</b> under /frontend to create it"
+# if on pythonanywhere
+if __name__ != '__main__':
+    print("not main")
 
-@app.route('/js/<path:path>')
-def send_js(path):
-    return app.send_static_file(os.path.join('js', path).replace('\\','/'))
+    @app.route('/')
+    def index():
+        try:
+            return render_template('index.html')
+        except:
+            return "There is not currently a /frontend/dist folder for the built frontend files. Type <b>npm run build</b> under /frontend to create it"
 
-@app.route('/css/<path:path>')
-def send_css(path):
-    return app.send_static_file(os.path.join('css', path).replace('\\','/'))
+    @app.route('/js/<path:path>')
+    def send_js(path):
+        return app.send_static_file(os.path.join('js', path).replace('\\', '/'))
 
-@app.route('/img/<path:path>')
-def send_img(path):
-    return app.send_static_file(os.path.join('img', path).replace('\\','/'))
+    @app.route('/css/<path:path>')
+    def send_css(path):
+        return app.send_static_file(os.path.join('css', path).replace('\\', '/'))
 
-@app.route('/fonts/<path:path>')
-def send_fonts(path):
-    return app.send_static_file(os.path.join('fonts', path).replace('\\','/'))
+    @app.route('/img/<path:path>')
+    def send_img(path):
+        return app.send_static_file(os.path.join('img', path).replace('\\', '/'))
 
-@app.route('/favicon.ico')
-def send_favicon():
-    return app.send_static_file('favicon.ico')
+    @app.route('/fonts/<path:path>')
+    def send_fonts(path):
+        return app.send_static_file(os.path.join('fonts', path).replace('\\', '/'))
+
+    @app.route('/favicon.ico')
+    def send_favicon():
+        return app.send_static_file('favicon.ico')
 
 #
 # API endpoints
 #
 
 # sanity check route
+
+
 @app.route('/ping', methods=['GET'])
 def ping_pong():
     return jsonify('test data')
 
 # example api endpoints
 # look at Theme.vue script for frontend example
+
+
 @app.route('/api/colors/primary', methods=['GET'])
 def get_primary_colors():
     return jsonify(neutral_colors)
+
 
 @app.route('/api/colors/accent', methods=['GET'])
 def get_accent_colors():
     return jsonify(accent_colors)
 
-
+# run server when local
+if __name__ == '__main__':
+    app.run()
