3348f5b7e2c39f6f1239c690a076e65d248ae422
dstepha3 <62953513+dstepha3@users.noreply.github.com>
2021-03-19 00:23:31 -0400

    adding my schedule bar prototype

---
+++ frontend/src/components/MiniSemester.vue
@@ -0,0 +1,119 @@
+<template lang="html">
+    <div class="semester">
+        <ul class="list-group">
+            <li class="list-group-item header">
+                <a @click="removeSemester()" 
+                @keyup.enter="removeSemester()" 
+                data-tooltip="Remove Semester"
+                data-tooltip-location="bottom">
+                <i class="fas fa-trash fa-sm padding"></i></a> 
+                <span id="semesterName" contenteditable="true">Spring 2021</span>
+                <span class="badge rounded-pill course-badge float-right">
+                    <button class="button-as-link" id="semesterCredits">16</button>
+                    </span></li>
+
+            <li class="list-group-item course">
+                <span class="badge rounded-pill course-badge">
+                    <button class="button-as-link">MATH 13013</button></span>
+                <span id="courseName">Analytic Geometry...</span></li>
+
+            <li class="list-group-item course">
+                <span class="badge rounded-pill course-badge">
+                    <button class="button-as-link">MATH 13013</button></span>
+                <span id="courseName">Analytic Geometry...</span></li>
+
+            <li class="list-group-item course">
+                <span class="badge rounded-pill course-badge">
+                    <button class="button-as-link">MATH 13013</button></span>
+                <span id="courseName">Analytic Geometry...</span></li>
+
+            <li class="list-group-item course">
+                <span class="badge rounded-pill course-badge">
+                    <button class="button-as-link">MATH 13013</button></span>
+                <span id="courseName">Analytic Geometry...</span></li>
+
+        </ul>
+    </div>
+</template>
+
+<script lang="js">
+import * as Toast from '../toast.js';
+
+export default {
+    name: 'miniSemester',
+    props: {
+      targetName: String
+    },
+    methods: {
+        removeSemester() {
+      var removePromptResult = confirm(
+        "Are you sure you want to remove this semester and all its courses?"
+      );
+      if (removePromptResult == true) {
+        Toast.showSuccessMessage(
+          "Semester was removed successfully."
+        );
+      }
+    },
+    }
+}
+</script>
+
+<style scoped>
+
+div.container {
+    padding: 10px;
+}
+
+.list-group {
+    width: 15rem;
+ }
+
+.list-group li {
+    background-color: var(--theme-blacker);
+    color: var(--theme-whiter);
+    overflow: hidden;
+}
+
+.list-group li.course {
+    height: 2.6rem;
+    font-size: 10pt;
+}
+
+.list-group-item.header {
+    font-size: 12pt;
+    color: var(--theme-whitest);
+}
+
+#courseName {
+    padding-left: 0.5rem;
+}
+
+span.course-badge {
+  color: var(--theme-whiter);
+  background-color: var(--theme-darkest-gray);
+}
+
+.course-badge button {
+  font: inherit;
+}
+
+.padding {
+  padding-right: 1rem;
+}
+
+.float-right {
+  float: right;
+  margin-top: 2px;
+}
+
+#semesterCredits {
+    font-size: 10pt;
+}
+
+.semester {
+    display: inline-block;
+    margin-right: 1rem;
+}
+
+</style>
---
+++ frontend/src/components/SemesterBar.vue
@@ -0,0 +1,114 @@
+
+<template lang="html">
+    <div>
+    <footer class="container">
+        <button class="semesterBar-button" v-on:click="toggle()">
+            <i class="far fa-calendar "></i>
+            <!-- My Schedule -->
+            </button>
+        <div class="semesterBar-container" v-show="isOpen" >
+            <span 
+            v-for="n in 4"
+            :key="n">
+                <MiniSemester></MiniSemester>
+            </span>
+            <div class="inline">
+                <a 
+                    v-if="!isRemovingCourse"
+                    tabindex="0"
+                    @keyup.enter="showAddSemesterModal()"
+                    @click="showAddSemesterModal()"
+                    data-tooltip="Add a Semester"
+                    data-tooltip-location="bottom"
+                    >
+                    <i class="fas fa-plus-circle fa-lg newSemester"></i>
+                    </a>
+          
+            </div>
+        </div>
+    </footer>
+    </div>
+</template>
+
+<script>
+
+import MiniSemester from '../components/MiniSemester.vue'
+
+export default ({
+    name: 'semester-bar',
+    data() {
+        return {
+            isOpen: false
+        }
+    },
+    methods: {
+        toggle: function() {
+            this.isOpen = !this.isOpen
+        }
+    },
+    components: {
+        MiniSemester
+    }
+})
+</script>
+
+
+<style scoped>
+
+.container {
+  width:99%;
+  position:fixed;
+  bottom:0;
+  z-index: 100;
+}
+
+.semesterBar-button {
+  cursor: pointer;
+  display: block;
+  min-width: 5rem;
+  margin-left: 0.90rem;
+  padding:15px 0;
+  background:var(--theme-black);
+  color:var(--theme-whiter);
+  border:0;
+  text-align:center;
+  border-top-left-radius:10px;
+  border-top-right-radius:10px;
+  border-top: solid 1pt var(--theme-primary-dark);
+}
+
+.semesterBar-container {
+  background: var(--theme-black);
+  color: var(--theme-whiter);
+  width:110%;
+  min-height:5rem;
+  text-align:left;
+  margin-left: -1rem;
+  border-top: solid 1pt var(--theme-primary-dark);
+  border-top-right-radius:10px;
+  overflow-x: auto;
+  overflow-y: hidden;
+  white-space: nowrap;
+  padding: 1rem;
+  -webkit-box-shadow: inset 0 0 6px rgba(0,0,0,0.3);
+  box-shadow: inset 0 0 6px rgba(0,0,0,0.3);
+}
+
+.inline {
+    display: inline-block;
+}
+
+.newSemester {
+    position: absolute;
+    top: 6rem;
+    padding-left: 1.5rem;
+    padding-right: 3rem;
+}
+
+@media only screen and (max-width:1280px) {
+    .container {
+        display: none;
+    }
+}
+
+</style>
