6c9165fc1f7b1b0dadc658f9c69b569c5ff16b34
Jaimie <jaimie6453@gmail.com>
2021-03-22 13:15:17 -0400

    pass fix

--- backend/flask_app.py
+++ backend/flask_app.py
@@ -4,9 +4,6 @@ from password import *
 
 on_pythonanywhere = not (__name__ == '__main__')
 
-print("site pass: ", ACCOUNT_PASS)
-print("db pass: ", DB_PASS)
-
 if on_pythonanywhere:    
     app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://KSUCoursePlanner.mysql.pythonanywhere-services.com'
     
@@ -14,17 +11,14 @@ if on_pythonanywhere:
 else:   
     tunnel = sshtunnel.SSHTunnelForwarder(
         ('ssh.pythonanywhere.com'),
-        ssh_username='KSUCoursePlanner', ssh_password=DB_PASS,
+        ssh_username='KSUCoursePlanner', ssh_password=ACCOUNT_PASS,
         remote_bind_address=('KSUCoursePlanner.mysql.pythonanywhere-services.com', 3306)
     )
 
     tunnel.start()
 
     # config to get in the main frame
-    conn = 'mysql://KSUCoursePlanner:{}@127.0.0.1:{}/KSUCoursePlanner$test'.format(ACCOUNT_PASS, tunnel.local_bind_port)
-    print(conn)
-    app.config['SQLALCHEMY_DATABASE_URI'] = conn
-    
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://KSUCoursePlanner:{}@127.0.0.1:{}/KSUCoursePlanner$test'.format(DB_PASS, tunnel.local_bind_port)
 
 # turn off depreciation warning
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
