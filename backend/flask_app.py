78283702151dac31545367b21402f4c9282c133e
ThatManTrey <tphillips_1969@yahoo.com>
2021-03-25 20:39:42 -0400

    transfered pythonanywhere changes to main

--- backend/flask_app.py
+++ backend/flask_app.py
@@ -5,6 +5,9 @@ from password import *
 on_pythonanywhere = not (__name__ == '__main__')
 
 if on_pythonanywhere:
+    print("------------- ON PYTHONANYWHERE ------------------\n")
+    print(os.environ)
+    
     SQLALCHEMY_DATABASE_URI = "mysql+mysqlconnector://{username}:{password}@{hostname}/{databasename}".format(
         username="KSUCoursePlanner",
         password=DB_PASS,
