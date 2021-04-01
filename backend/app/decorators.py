0fb5ddc02ffd81cfa22a4c689774c0c77c224a68
Jaimie <jaimie6453@gmail.com>
2021-04-01 14:18:19 -0400

    removing api key decorator, adding optional to access token check

--- backend/app/decorators.py
+++ backend/app/decorators.py
@@ -23,28 +23,11 @@ def is_current_user(function):
     return wrapper
 
 
-# def has_api_key():
-#     def wrapper(fn):
-#         @wraps(fn)
-#         def decorator(*args, **kwargs):
-#             if os.environ['FLASK_ENV'] == "development":
-#                 return fn(*args, **kwargs)
-            
-#             if "Api-Key" not in request.headers:
-#                 return "API Key is required", HTTPStatus.INTERNAL_SERVER_ERROR
-#             elif request.headers["Api-Key"] != app.config['SECRET_KEY']:
-#                 return "Invalid API Key", HTTPStatus.INTERNAL_SERVER_ERROR
-#             else:
-#                  return fn(*args, **kwargs)
-#         return decorator
-#     return wrapper
-
-
-def has_access_token():
+def has_access_token(optional=False):
     def wrapper(fn):
         @wraps(fn)
         def decorator(*args, **kwargs):
-            verify_jwt_in_request()
+            verify_jwt_in_request(optional)
             
             if get_jwt()['type'] == "access":
                 return fn(*args, **kwargs)
