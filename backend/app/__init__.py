fac38701a535ec9ea19e2ad5da54888cce4c4d79
Jaimie <jaimie6453@gmail.com>
2021-04-04 21:45:14 -0400

    converting secret key to bytes

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -30,7 +30,7 @@ jwt = JWTManager(app)
 
 if os.environ['FLASK_ENV'] == "production":
     # Initial page rendering needed for PythonAnywhere
-    # requires npm run build to be run in /frontend first\
+    # requires npm run build to be run in /frontend first
     @app.route('/')
     def index():
         try:
@@ -38,25 +38,28 @@ if os.environ['FLASK_ENV'] == "production":
         except:
             return "There is not currently a /frontend/dist folder for the built frontend files. Type <b>npm run build</b> under /frontend to create it"
 
-
     def verify_signature(request):
         print("verifying signature....")
-        received_signature = request.headers["X-Hub-Signature-256"].split("sha256=")[1]
+        received_signature = request.headers["X-Hub-Signature-256"].split("sha256=")[
+            1]
         print("received signature: ", received_signature)
         if received_signature is None:
             return HTTPStatus.BAD_REQUEST
-        
-        expected_signature = HMAC(key=app.config['SECRET_KEY'], msg=request.data, digestmod=sha256).hexdigest()
+
+        key_as_bytes = bytes(app.config['SECRET_KEY'], 'utf-8')
+        print("key as bytes: ", key_as_bytes)
+        expected_signature = HMAC(
+            key=key_as_bytes, msg=request.data, digestmod=sha256).hexdigest()
         print("expected signature: ", expected_signature)
         return compare_digest(received_signature, expected_signature)
 
-
     # require API key for each request on pythonanywhere
+
     @app.before_request
-    def before_request_func():   
+    def before_request_func():
         if request.endpoint == "index" or request.endpoint == "update_server":
             return
-        
+
         api_key = request.headers["Api-Key"]
         if api_key is None:
             return "API Key is required", HTTPStatus.BAD_REQUEST
@@ -64,8 +67,8 @@ if os.environ['FLASK_ENV'] == "production":
         if api_key != app.config['SECRET_KEY']:
             return "Invalid API Key", HTTPStatus.BAD_REQUEST
 
-
     # for server updates
+
     @app.route('/update_server', methods=['POST'])
     def update_server():
         if request.method == 'POST':
@@ -83,5 +86,4 @@ if os.environ['FLASK_ENV'] == "production":
             return 'Wrong event type', HTTPStatus.BAD_REQUEST
 
 
-from app import auth, api   
-     
+from app import auth, api
