5f04f514dee866d8cfb081d2322ec5fc579c6db2
dstepha3 <62953513+dstepha3@users.noreply.github.com>
2021-04-06 10:33:38 -0400

    implementing user favorites courses

--- frontend/src/views/Favorites.vue
+++ frontend/src/views/Favorites.vue
@@ -1,20 +1,28 @@
 <template lang="html">
   <div>
     <ThemeNavBar></ThemeNavBar>
+    <PageSpinner
+      v-if="!hasLoadedCourses"
+      :showSpinner="!hasLoadedCourses"
+    ></PageSpinner>
+
     <div class="container">
       <FilterCoursesBar></FilterCoursesBar>
-      <div class="row course-container">
-        <div
-          class="col-sm-12 col-md-6 col-lg-4 col-xl-3 mb-3"
-          v-for="n in 8"
-          :key="n"
-        >
-          <CourseCard
-            @openAddSemesterModal="showAddToSemesterModal"
-            @openCourseInfoModal="showCourseInfoModal"
-          ></CourseCard>
+        <transition name="coursefade">
+          <div v-show="hasLoadedCourses" class="row mx-3">
+            <div
+              class="col-sm-12 col-md-6 col-lg-4 col-xl-3 mb-3"
+              v-for="(course, index) in courses.deptCourses"
+              :key="index"
+            >
+            <CourseCard
+              @openAddSemesterModal="showAddToSemesterModal"
+              @openCourseInfoModal="showCourseInfoModal"
+              v-bind:course="course"
+            ></CourseCard>
+          </div>
         </div>
-      </div>
+      </transition>
     </div>
 
     <CourseInfoModal ref="courseInfoModalFavorites"></CourseInfoModal>
@@ -25,9 +33,11 @@
 <script lang="js">
 import ThemeNavBar from '../components/ThemeNavBar.vue';
 import CourseCard from '../components/CourseCard.vue';
+import PageSpinner from '../components/spinners/PageSpinner.vue';
 import CourseInfoModal from '../components/modals/CourseInfoModal.vue';
 import AddToSemesterModal from '../components/modals/AddToSemesterModal.vue'
 import FilterCoursesBar from '../components/FilterCoursesBar.vue';
+import axios from 'axios';
 
 export default {
     name: 'favorites',
@@ -35,22 +45,44 @@ export default {
     components: {
         ThemeNavBar,
         CourseCard,
+        PageSpinner,
         CourseInfoModal,
         AddToSemesterModal,
         FilterCoursesBar
     },
-
-data () {
+    created() {
+        // loading test
+        this.getCourses();
+    },
+    
+    data () {
       return {
         isModalVisible: false,
+        courses: [],
+        hasLoadedCourses: false
       };
     },
+
       methods: {
         showCourseInfoModal () {
             this.$refs.courseInfoModalFavorites.openModal();
         },
         showAddToSemesterModal () {
             this.$refs.addToSemesterModalFavorites.openModal();
+        },
+        getCourses() {
+          var baseUrl = process.env.VUE_APP_API_URL + "/user/" + this.$store.state.userId
+
+          //AJAX request
+          axios.get(baseUrl + "/favorites")
+            .then((res) => {
+              this.courses = res.data;
+              this.hasLoadedCourses = true;
+            })
+            .catch((error) => {
+              // eslint-disable-next-line
+              console.error(error);
+            });
         }
       },
 };
