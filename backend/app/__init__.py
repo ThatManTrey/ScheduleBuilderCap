d316ed116b78b5a727754865f92efc031d514077
dstepha3 <62953513+dstepha3@users.noreply.github.com>
2021-04-06 14:16:14 -0400

    manual update from API branch

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,13 +1,14 @@
 from flask import Flask, render_template, request
 from flask_cors import CORS
-from flask_sqlalchemy import SQLAlchemy 
+from flask_sqlalchemy import SQLAlchemy
 from flask_jwt_extended import JWTManager
 from flask_mail import Mail
 from config import DevelopmentConfig, ProductionConfig
+from hmac import HMAC, compare_digest
+from hashlib import sha256
 import os
 
 from http import HTTPStatus
-import git
 
 # points to the built files folder for Vue
 app = Flask(__name__, template_folder="../../frontend/dist/")
@@ -15,13 +16,12 @@ app = Flask(__name__, template_folder="../../frontend/dist/")
 if os.environ['FLASK_ENV'] == "development":
     app.config.from_object(DevelopmentConfig)
 else:
+    import git
     app.config.from_object(ProductionConfig)
 
 
 db = SQLAlchemy(app)
 
-from app import scraper
-# scraper.start_scraper()
 
 CORS(app, resources={r'/*': {'origins': '*'}}, supports_credentials=True)
 mail = Mail(app)
@@ -38,26 +38,49 @@ if os.environ['FLASK_ENV'] == "production":
         except:
             return "There is not currently a /frontend/dist folder for the built frontend files. Type <b>npm run build</b> under /frontend to create it"
 
+
+    def verify_signature(request):
+        print("verifying signature....")
+        received_signature = request.headers["X-Hub-Signature-256"].split("sha256=")[1]
+        print("received signature: ", received_signature)
+        if received_signature is None:
+            return HTTPStatus.BAD_REQUEST
+        
+        expected_signature = HMAC(key=app.config['SECRET_KEY'], msg=request.data, digestmod=sha256).hexdigest()
+        print("expected signature: ", expected_signature)
+        return compare_digest(received_signature, expected_signature)
+
+
     # require API key for each request on pythonanywhere
     @app.before_request
-    def before_request_func():
-        if "Api-Key" not in request.headers:
-            return "API Key is required", HTTPStatus.INTERNAL_SERVER_ERROR
-        elif request.headers["Api-Key"] != app.config['SECRET_KEY']:
-            return "Invalid API Key", HTTPStatus.INTERNAL_SERVER_ERROR
+    def before_request_func():   
+        if request.endpoint == "index" or request.endpoint == "update_server":
+            return
+        
+        api_key = request.headers["Api-Key"]
+        if api_key is None:
+            return "API Key is required", HTTPStatus.BAD_REQUEST
 
-# for server updates
-@app.route('/update_server', methods=['POST'])
-def webhook():
-    if request.method == 'POST':
-        repo = git.Repo('/home/KSUCoursePlanner/ScheduleBuilderCap')
-        origin = repo.remotes.origin
-    
-        origin.pull()
-
-        return 'Updated PythonAnywhere successfully', 200
-    else:
-        return 'Wrong event type', 400
+        if api_key != app.config['SECRET_KEY']:
+            return "Invalid API Key", HTTPStatus.BAD_REQUEST
 
 
-from app import auth, api
+    # for server updates
+    @app.route('/update_server', methods=['POST'])
+    def update_server():
+        if request.method == 'POST':
+            if verify_signature(request):
+                repo = git.Repo('/home/KSUCoursePlanner/ScheduleBuilderCap')
+                origin = repo.remotes.origin
+
+                origin.pull()
+                print("if this is in the server log it should work")
+                return 'Updated PythonAnywhere successfully'
+            else:
+                print("signature is not valid")
+                return "Incorrect signature", HTTPStatus.FORBIDDEN
+        else:
+            return 'Wrong event type', HTTPStatus.BAD_REQUEST
+
+
+from app import auth, api   
